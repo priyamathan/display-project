6eabc5ab599a484ec4625c90584d176e428f3f21
Priya <vpriyamathan01@gmail.com>
2018-09-30 17:47:33 -0600

    second commit

--- Display/src/DisplayText.java
+++ Display/src/DisplayText.java
@@ -4,6 +4,7 @@ public class DisplayText {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Maven Example");
+//second commit!
+System.out.println("git example");
 	}
-
 }
